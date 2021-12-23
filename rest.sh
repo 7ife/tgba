98d253486e8909258fd5d2be416cb36aa3462d4f
Life <iserje@icloud.com>
2021-12-23 19:46:51 +0600

    Add files via upload

---
+++ rest.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+#Coded by 7ife
+clear
+echo ""
+echo -e "\033[1;92m ‎"
+echo "${k}█▄─█▀▀▀█─▄█▄─▄▄─█▄─▄███─▄▄▄─█─▄▄─█▄─▀█▀─▄█▄─▄▄─█";
+echo "${k}██─█─█─█─███─▄█▀██─██▀█─███▀█─██─██─█▄█─███─▄█▀█";
+echo "${k}▀▀▄▄▄▀▄▄▄▀▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀▄▄▄▀▄▄▄▀▄▄▄▄▄▀";
+echo ""
+echo "${k}█▄─▄─▀██▀▄─██─▄▄▄─█▄─█─▄█";
+echo "${k}██─▄─▀██─▀─██─███▀██─▄▀██";
+echo "${k}▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀";
+echo ""
+echo -e "\033[1;90m  Github: github.com/7ife"
+echo -e "\033[1;90m  E-mail: 7ife@pm.me"
+echo ""
+echo -e $'\e[1;34m::TGBA::\e[0m\e[1;32m #Bot launch\e[0m'
+sleep 2
+echo -e " \033[1;93m Сongratulations, now you can check your bot"
+echo ""
+npm start
+echo ""
+sleep 2
+echo -e $'\e[1;34m::TGBA::\e[0m\e[0;31m #if you see this message, then the installation did not work, be attentive to the steps, delete and install the termux again and repeat the steps\e[0m'
+echo ""
+while true
+do
+sleep 5
+done
