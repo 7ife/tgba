de43d68eaed1d6b7a4b38756100d1fc03fb72d7d
Life <iserje@icloud.com>
2021-12-25 01:11:37 +0600

    Update rest.sh

--- rest.sh
+++ rest.sh
@@ -16,12 +16,9 @@ echo -e "\033[1;90m  E-mail: 7ife@pm.me"
 echo ""
 echo -e $'\e[1;34m::TGBA::\e[0m\e[1;32m #Bot launch\e[0m'
 sleep 2
-echo -e " \033[1;93m Сongratulations, now you can check your bot"
+echo -e " \033[1;93m Start Bot"
 echo ""
 npm start
-echo ""
-sleep 2
-echo -e $'\e[1;34m::TGBA::\e[0m\e[0;31m #if you see this message, then the installation did not work, be attentive to the steps, delete and install the termux again and repeat the steps\e[0m'
 echo ""
 while true
 do
